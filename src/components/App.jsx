387ca739abb67d9c789b9766435afb3387fd77ea
Mykhaylo Yaremkevych <114567561+Mykhaylo33@users.noreply.github.com>
2023-06-05 21:58:14 +0300

    changed structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import FeedbackTitle from './Feedback/FeedbackTitle/FeedbackTitle';
-import FeedbackStatistics from './Feedback/FeedbackStatistics';
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 
-export const App = () => {
-  return (
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-    <div
-      style={{
-        height: '100vh',
-        justifyContent: 'center',
-        alignItems: 'center',
-        color: '#010101',
-        fontSize: 26,
-      }}
-    >
 
-    <FeedbackTitle title='Please leave feedback'style={{marginLeft: 30}}/>
+  handleButtonClick = e => {
+    const { name } = e.currentTarget;
 
-    <FeedbackStatistics/>
-    
-    </div>
-  );
-};
+    this.setState(prevState => {
+      return {
+        [name]: prevState[name] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Number(
+      (
+        (this.state.good * 100) /
+        (this.state.good + this.state.neutral + this.state.bad)
+      ).toFixed(0)
+    );
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleButtonClick}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
--- src/components/Feedback/Notification/Notification.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Notification = ({message, styles}) => {
-    return(
-        <p style={styles} >{message}</p>
-    )
-}
-
-export default Notification;
